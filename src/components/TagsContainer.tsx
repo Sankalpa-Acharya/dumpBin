3423c224b372dd3e2b9131b759256ad4ee164c2f
sankalpa-acharya <sankalpaacharya6609@gmail.com>
2024-02-02 18:21:48 +0530

    added: added opearting system tag

--- src/components/TagsContainer.tsx
+++ src/components/TagsContainer.tsx
@@ -1,15 +1,16 @@
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-import { Code, Target, Database } from 'lucide-react';
+import { Code, Target, Database, Laptop } from 'lucide-react';
 
 type Props = {}
 
 export default function TagsContainer({ }: Props) {
 
     return (
-        <ToggleGroup onValueChange={(e) => console.log(e)} type="single" className="flex flex-wrap">
+        <ToggleGroup onValueChange={(e) => console.log(e)} type="single" className="flex flex-wrap justify-start">
             <ToggleGroupItem value="daa" className="flex gap-3"><Target size={18} /> DAA</ToggleGroupItem>
             <ToggleGroupItem value="designpattern" className="flex gap-3"><Code size={18} /> Design Pattern</ToggleGroupItem>
-            <ToggleGroupItem value="database" className="flex gap-3"><Database size={18} /> Database</ToggleGroupItem>
+            <ToggleGroupItem value="database" className="flex gap-3"><Database size={18} /> DBMS</ToggleGroupItem>
+            <ToggleGroupItem value="database" className="flex gap-3"><Laptop size={18} /> OS</ToggleGroupItem>
         </ToggleGroup>
 
     )
